66f68df339ab334cef5734b823ac747284b06bf5
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-21 23:54:57 -0700

    working on app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 const App = () => {
-  return <h1>Hiiiii</h1>
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const handleClick = () => {
+    console.log(input)
+  };
+
+  return (
+    <div>
+      <textarea 
+        onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
 };
 
 ReactDOM.render(
