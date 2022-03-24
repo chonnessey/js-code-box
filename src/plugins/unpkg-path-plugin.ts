f7f245cb0e48df46f71943a4e86c66e2a47d09f6
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-23 18:30:32 -0700

    fix logic to dynamically create URL

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,10 +14,10 @@ export const unpkgPathPlugin = () => {
         if(args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           }
         }
-        
+
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
@@ -35,16 +35,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        // console.log(request)
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
       });
     },
