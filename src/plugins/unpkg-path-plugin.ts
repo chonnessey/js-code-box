d919adfaa7196ba4c3efe93b8d6e375003e8a021
Adrian Camacho <adriancamacho18@gmail.com>
2022-03-23 18:41:35 -0700

    test out different modules from npm

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -35,7 +35,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
+              const message = require('lodash');
               console.log(message);
             `,
           };
