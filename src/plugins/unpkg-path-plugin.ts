a1f28471f81b550c504f7a5b40d46753f185145e
Adrian Camacho <adriancamacho18@gmail.com>
2022-04-08 12:14:37 -0700

    refactor if statements in plugin file to their own functions

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,31 +6,30 @@ const fileCache = localForage.createInstance({
   name: 'filecache'
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      //handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+        };
+      });
+
+      //handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if(args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        } 
-        
-        if(args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-          }
-        }
 
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         }
-        
-        // else if(args.path === 'tiny-test-pkg') {
-        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
-        // }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -39,10 +38,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react-select';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         }
 
